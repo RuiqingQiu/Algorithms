c391f6dee0fa10df324f615f04081c0504171085
Ruiqing Qiu <ruiqingqiu@Ruiqings-MacBook-Pro-2.local>
2017-10-26 00:50:51 -0700

    find k pairs with smallest sums done using priority queue

--- findKPairsWithSmallestSums.java
+++ findKPairsWithSmallestSums.java
@@ -1,4 +1,22 @@
 class Solution{
-    public List<int[]> kSmallestPairs(int[] nums, int nums2, int k){
+    public List<int[]> kSmallestPairs(int[] nums1, int nums2, int k){
+        List<int[]> result = new ArrayList<int[]>();
+        if(nums1.length == 0 || nums2.length == 0 || k == 0){
+            return result;
+        }
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(
+                (a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        for(int i = 0; i < nums1.length; i++){
+            queue.offer(new int[]{nums1[i], nums2[0], 0});
+        }
+        while(k-- > 0 && !queue.isEmpty()){
+            int[] cur = queue.poll();
+            result.add(new int[]{cur[0], cur[1]});
+            if(cur[2] == nums[2].length - 1){
+                continue;
+            }
+            queue.offer(new int[]{cur[0], nums2[cur[2]+1], cur[2] + 1});
+        }
+        return result;
     }
 }
