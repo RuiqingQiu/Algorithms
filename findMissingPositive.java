8a91303afccb9c1c488d89c098a3aefb16aebde9
Ruiqing Qiu <ruiqingqiu@Ruiqings-MacBook-Pro-2.local>
2017-10-25 23:47:51 -0700

    find missing positive

--- findMissingPositive.java
+++ findMissingPositive.java
@@ -1,6 +1,28 @@
-Class Solution{
+class Solution{
 
   public int firstMissingPositive(int[] nums){
+      int j = 0;
+      for(int i = 0; i < nums.length; i++){
+         if(nums[i] <= 0){
+             int tmp = nums[i];
+             nums[i] = nums[j];
+             nums[j] = nums[i];
+             j++;
+         }
+      }
+      int size = nums.length - j - 1;
+      int [] test_arr = new int[size];
+      for(int i = j; i < nums.length; i++){
+        if(nums[i] <= size){
+            test_arr[nums[i]-1] = -1; 
+        }
+      }
+      for(int i = 0; i < size; i++){
+        if(test_arr[i] >= 0){
+            return i + 1; 
+        }
+      }
+      return size + 1;
   }
 
 }
