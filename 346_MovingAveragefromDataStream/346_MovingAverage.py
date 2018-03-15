6017127fc362bdbebcfe8e54ed1bb8425ae347f8
Ruiqing Qiu <rqiu@ucinnovation.com>
2018-03-14 18:36:04 -0700

    346 added

---
+++ 346_MovingAveragefromDataStream/346_MovingAverage.py
@@ -0,0 +1,26 @@
+class MovingAverage:
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.queue = []
+        self.window_size = size
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        self.queue.append(val)
+        if len(self.queue) > self.window_size:
+            self.queue.pop(0)
+            return sum(self.queue) / self.window_size
+        else:
+            return sum(self.queue) / len(self.queue)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
