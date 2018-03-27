711d434950dc21edba3f500c2def245919e77267
Ruiqing Qiu <rqiu@ucinnovation.com>
2018-03-27 16:49:19 -0700

    805 python 3 solution

--- 805_SplitArrayWithSameAverage/805_splitArraySameAverage.py
+++ 805_SplitArrayWithSameAverage/805_splitArraySameAverage.py
@@ -1,7 +1,24 @@
-class Solution:
+class Solution():
     def splitArraySameAverage(self, A):
-        """
-        :type A: List[int]
-        :rtype: bool
-        """
-        
+        from fractions import Fraction
+        N = len(A)
+        S = sum(A)
+        A = [z - Fraction(S, N) for z in A]
+
+        if N == 1: return False
+
+        #Want zero subset sum
+        left = {A[0]}
+        for i in range(1, int(N / 2)):
+            left = {z + A[i] for z in left} | left | {A[i]}
+        if 0 in left: return True
+
+        right = {A[-1]}
+        for i in range(int(N / 2), N-1):
+            right = {z + A[i] for z in right} | right | {A[i]}
+        if 0 in right: return True
+
+        sleft = sum(A[i] for i in range(int(N / 2)))
+        sright = sum(A[i] for i in range(int(N / 2), N))
+
+        return any(-ha in right and (ha, -ha) != (sleft, sright) for ha in left)
