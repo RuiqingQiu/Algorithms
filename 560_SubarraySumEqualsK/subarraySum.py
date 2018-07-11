6ed80db95843dff39397af0be70c0fa90d62cb2f
Ruiqing Qiu <rqiu@ucinnovation.com>
2018-07-10 19:07:38 -0700

    maximum subarray and subarray sum equals k

---
+++ 560_SubarraySumEqualsK/subarraySum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = collections.Counter()
+        count[0] = 1
+        ans = sum = 0
+
+        for num in nums:
+            sum += num
+            ans += count[sum - k]
+            count[sum] += 1
+
+        return ans
