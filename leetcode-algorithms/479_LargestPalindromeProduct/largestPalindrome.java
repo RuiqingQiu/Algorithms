f1dc7246f258cdaa1ac76145a966f11c86344edc
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2018-10-01 19:15:53 -0700

    479 permutate largest number whose palidrome

--- leetcode-algorithms/479_LargestPalindromeProduct/largestPalindrome.java
+++ leetcode-algorithms/479_LargestPalindromeProduct/largestPalindrome.java
@@ -1,51 +1,9 @@
 class Solution {
-//     public int largestPalindrome(int n) {
-//         int num1 = 0;
-//         int num2 = 0;
-//         long max = -1;
-
-//         for (int i = 0; i < n; i++) {
-//             num1 = num1 * 10 + 9;
-//         }
-
-//         int min = (n - 1) * 10;
-
-//         for (long i = num1; i >= min; i--) {
-//             for (long j = num1; j >= min; j--) {
-//                 if (i * j <= max) {
-//                     break;
-//                 }
-
-//                 if (isPalindrome(i * j)) {
-//                     max = Math.max(max, i * j);
-//                     break;
-//                 }
-//             }
-//         }
-
-//         if (max == -1) {
-//             return -1;
-//         } else {
-//             return (int)(max % 1337);
-//         }
-//     }
-
-//     private boolean isPalindrome(long n) {
-//         String str = Long.toString(n);
-
-//         for (int i = 0; i < str.length() / 2; i++) {
-//             if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-
-//     }
     public int largestPalindrome(int n) {
         long max = (long)Math.pow(10, n) - 1;
         long min = max / 10;
 
+        // The idea is to find the largest number of palidrome and check whether the number can be divided by
         for (long h = max; h > min; h--) {
             long left = h, right = 0;
 
