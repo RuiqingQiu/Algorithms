7404fb52c4a9bea1e96cca612dac3c2c34e3cef7
RuiqingQiu <raymond.qiu1993@gmail.com>
2018-10-08 21:31:11 -0700

    hamming distance

---
+++ leetcode-algorithms/461_HammingDistance/hammingDistance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+
+        while (x > 0 || y > 0) {
+            if (x % 2 != y % 2) {
+                result += 1;
+            }
+
+            x = x / 2;
+            y = y / 2;
+        }
+
+        return result;
+    }
+}
