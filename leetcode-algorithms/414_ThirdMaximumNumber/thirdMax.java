e7b10b3b5c8daf5c18247eb39b5de35c270cb3c6
RuiqingQiu <raymond.qiu1993@gmail.com>
2018-10-17 22:38:33 -0700

    added two problems

---
+++ leetcode-algorithms/414_ThirdMaximumNumber/thirdMax.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(3);
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                continue;
+            } else {
+                if (pq.size() == 3) {
+                    if (num > pq.peek()) {
+                        int removed = pq.poll();
+                        pq.offer(num);
+                        set.remove(removed);
+                        set.add(num);
+                    }
+                } else {
+                    pq.offer(num);
+                    set.add(num);
+                }
+
+            }
+        }
+
+        if (pq.size() == 3) {
+            return pq.poll();
+        } else {
+            int result = 0;
+
+            while (!pq.isEmpty()) {
+                result = pq.poll();
+            }
+
+            return result;
+        }
+
+    }
+}
